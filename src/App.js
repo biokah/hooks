a70bb8452ae80b4fc27748edad92aecea14d89dd
Maria Anais Reyes <maria.anaisr@gmail.com>
2022-12-21 20:56:29 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
+// import Styles
 import './App.css';
 
+// import from library
+import React, {useState, useRef, useEffect} from 'react';
+
+// components from Bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
+
+
 function App() {
+
+  const [counter, setCounter] = useState(0)
+  const [isLoggedIn, setLogIn] = useState(false)
+  const [string, setString] = useState("")
+  const exampleRef = useRef(null)
+
+  const modules = ["maquetado", "javascript", "backend", "react", "cloud"]
+  useEffect(()=>{
+    fetch("https://randomuser.me/api/?results=30")
+    .then((response) => response.json())
+    .then((data) => console.log(data));
+  }, [])
+
+  const add = () => {
+    setCounter(counter + 1)
+  }
+
+  const substract = () => {
+    if(counter > 0) {
+      setCounter(counter - 1)
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+
+      <main>
+        <section>
+          <Button variant="primary" onClick={substract}>-</Button>
+          <span>{counter}</span>
+          <Button variant="primary" onClick={add}>+</Button>
+          <Button variant="success" onClick={()=>{exampleRef.current.focus()}}>Log in </Button>
+          { isLoggedIn ? <span>Log out</span> : <span>Log in</span> }
+      </section>
+      <div>
+       
+        <input value={string} ref={exampleRef} onChange={(e)=>{setString(e.target.value)}}/>
+        String: {string}
+        <ul>
+        {modules
+        .filter(item => item.includes(string))
+        .map(item => <li key={item}>{item}</li>)
+        }
+        </ul> 
+      </div>
+      </main> 
+
+      
+      </div>
+    
   );
 }
+
+// https://randomuser.me/api/?results=30
 
 export default App;
